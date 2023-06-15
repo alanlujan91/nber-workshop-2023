6c50e305e0ba933b6f7c4705fb6b78cf24c54f28
Alan Lujan <alanlujan91@gmail.com>
2023-06-15 12:39:42 -0400

    clean up

--- Lectures/sim_steady_state_fast.py
+++ Lectures/sim_steady_state_fast.py
@@ -16,43 +16,56 @@ import numpy as np
 import numba
 
 # little need to speed up these functions
-from sim_steady_state import (discretize_assets, rouwenhorst_Pi, forward_iteration,
-                              expectation_iteration, expectation_functions)
+from sim_steady_state import (
+    discretize_assets,
+    rouwenhorst_Pi,
+    forward_iteration,
+    expectation_iteration,
+    expectation_functions,
+)
 
 """Part 0: example calibration from notebook"""
 
+
 # NO CHANGE
 def example_calibration():
-    y, _, Pi = discretize_income(0.975, 0.7, 7) 
-    return dict(a_grid = discretize_assets(0, 10_000, 500),
-                y=y, Pi=Pi,
-                r = 0.01/4, beta=1-0.08/4, eis=1)
+    y, _, Pi = discretize_income(0.975, 0.7, 7)
+    return dict(
+        a_grid=discretize_assets(0, 10_000, 500),
+        y=y,
+        Pi=Pi,
+        r=0.01 / 4,
+        beta=1 - 0.08 / 4,
+        eis=1,
+    )
 
 
 """Part 1: discretization tools"""
 
+
 # NO CHANGE
 def discretize_income(rho, sigma, n_e):
     # choose inner-switching probability p to match persistence rho
-    p = (1+rho)/2
-    
+    p = (1 + rho) / 2
+
     # start with states from 0 to n_e-1, scale by alpha to match standard deviation sigma
     e = np.arange(n_e)
-    alpha = 2*sigma/np.sqrt(n_e-1)
-    e = alpha*e
-    
+    alpha = 2 * sigma / np.sqrt(n_e - 1)
+    e = alpha * e
+
     # obtain Markov transition matrix Pi and its stationary distribution
     Pi = rouwenhorst_Pi(n_e, p)
     pi = stationary_markov(Pi)
-    
+
     # e is log income, get income y and scale so that mean is 1
     y = np.exp(e)
     y /= np.vdot(pi, y)
-    
+
     return y, pi, Pi
 
 
 """Support for part 1: equality testing and Markov chain convergence"""
+
 
 @numba.njit
 def equal_tolerance(x1, x2, tol):
@@ -69,11 +82,11 @@ def equal_tolerance(x1, x2, tol):
 
 
 @numba.njit
-def stationary_markov(Pi, tol=1E-14):
+def stationary_markov(Pi, tol=1e-14):
     # start with uniform distribution over all states
     n = Pi.shape[0]
-    pi = np.full(n, 1/n)
-    
+    pi = np.full(n, 1 / n)
+
     # update distribution using Pi until successive iterations differ by less than tol
     for it in range(10_000):
         pi_new = Pi.T @ pi
@@ -86,61 +99,63 @@ def stationary_markov(Pi, tol=1E-14):
 
 """Part 2: Backward iteration for policy"""
 
+
 def backward_iteration(Va, Pi, a_grid, y, r, beta, eis):
     # step 1: discounting and expectations
     Wa = beta * Pi @ Va
-    
+
     # step 2: solving for asset policy using the first-order condition
-    c_endog = Wa**(-eis)
-    coh = y[:, np.newaxis] + (1+r)*a_grid
+    c_endog = Wa ** (-eis)
+    coh = y[:, np.newaxis] + (1 + r) * a_grid
     # SPEEDUP: interpolation exploits monotonicity and avoids pure-Python for loop
     a = interpolate_monotonic_loop(coh, c_endog + a_grid, a_grid)
-        
+
     # step 3: enforcing the borrowing constraint and backing out consumption
     # SPEEDUP: enforce constraint in place, without creating new arrays, stop when unnecessary
     setmin(a, a_grid[0])
     c = coh - a
-    
+
     # step 4: using the envelope condition to recover the derivative of the value function
-    Va = (1+r) * c**(-1/eis)
-    
+    Va = (1 + r) * c ** (-1 / eis)
+
     return Va, a, c
 
 
-def policy_ss(Pi, a_grid, y, r, beta, eis, tol=1E-9):
+def policy_ss(Pi, a_grid, y, r, beta, eis, tol=1e-9):
     # initial guess for Va: assume consumption 5% of cash-on-hand, then get Va from envelope condition
-    coh = y[:, np.newaxis] + (1+r)*a_grid
+    coh = y[:, np.newaxis] + (1 + r) * a_grid
     c = 0.05 * coh
-    Va = (1+r) * c**(-1/eis)
-    
+    Va = (1 + r) * c ** (-1 / eis)
+
     # iterate until maximum distance between two iterations falls below tol, fail-safe max of 10,000 iterations
     for it in range(10_000):
         Va, a, c = backward_iteration(Va, Pi, a_grid, y, r, beta, eis)
-        
+
         # after iteration 0, can compare new policy function to old one
         # SPEEDUP: only test for convergence every 10 iterations
         # SPEEDUP: use equal_tolerance rather than inefficient NumPy test
         if it % 10 == 1 and equal_tolerance(a, a_old, tol):
             return Va, a, c
-        
+
         a_old = a
 
 
 """Support for part 2: equality testing and Markov chain convergence"""
+
 
 @numba.njit
 def interpolate_monotonic(x, xp, yp):
     """Linearly interpolate the data points (xp, yp) and evaluate at x, with both x and xp monotonic"""
     nx, nxp = x.shape[0], xp.shape[0]
     y = np.empty(nx)
-    
+
     # at any given moment, we are looking between data points with indices xp_i and xp_i+1
     # we'll keep track of xp_i and also the values of xp at xp_i and xp_i+1
     # (note: if x is outside range of xp, we'll use closest data points and extrapolate)
     xp_i = 0
     xp_lo = xp[xp_i]
     xp_hi = xp[xp_i + 1]
-    
+
     # iterate through all points in x
     for xi_cur in range(nx):
         x_cur = x[xi_cur]
@@ -148,7 +163,7 @@ def interpolate_monotonic(x, xp, yp):
             # if current x (x_cur) is below upper data point (xp_hi), we're good
             if x_cur < xp_hi:
                 break
-                
+
             # otherwise, we need to look at the next pair of data points until x_cur is less than xp_hi
             # (so between xp_lo and xp_hi)
             xp_i += 1
@@ -157,7 +172,7 @@ def interpolate_monotonic(x, xp, yp):
 
         # find the pi such that x_cur = pi*x_lo + (1-pi)*x_hi
         pi = (xp_hi - x_cur) / (xp_hi - xp_lo)
-        
+
         # use this pi to interpolate the y
         y[xi_cur] = pi * yp[xp_i] + (1 - pi) * yp[xp_i + 1]
     return y
@@ -188,21 +203,23 @@ def setmin(x, xmin):
 
 """Part 3: forward iteration for distribution"""
 
+
 @numba.njit
 def interpolate_lottery(x, xp):
     """Given a grid of xp, for each entry x_cur in (increasing) x, find the i and pi
-    such that x_cur = pi*xp[i] + (1-pi)*xp[i+1], where xp[i] and xp[i+1] bracket x_cur"""
+    such that x_cur = pi*xp[i] + (1-pi)*xp[i+1], where xp[i] and xp[i+1] bracket x_cur
+    """
     nx, nxp = x.shape[0], xp.shape[0]
     i = np.empty(nx, dtype=np.int64)
     pi = np.empty(nx)
-    
+
     # at any given moment, we are looking between data points with indices xp_i and xp_i+1
     # we'll keep track of xp_i and also the values of xp at xp_i and xp_i+1
     # (note: if x is outside range of xp, we'll use closest data points and extrapolate)
     xp_i = 0
     xp_lo = xp[xp_i]
     xp_hi = xp[xp_i + 1]
-    
+
     # iterate through all points in x
     for xi_cur in range(nx):
         x_cur = x[xi_cur]
@@ -210,7 +227,7 @@ def interpolate_lottery(x, xp):
             # if current x (x_cur) is below upper data point (xp_hi), we're good
             if x_cur < xp_hi:
                 break
-                
+
             # otherwise, we need to look at the next pair obf data points until x_cur is less than xp_hi
             # (so between xp_lo and xp_hi)
             xp_i += 1
@@ -232,13 +249,13 @@ def interpolate_lottery_loop(x, xp):
     return i, pi
 
 
-def distribution_ss(Pi, a, a_grid, tol=1E-10):
+def distribution_ss(Pi, a, a_grid, tol=1e-10):
     a_i, a_pi = interpolate_lottery_loop(a, a_grid)
-    
+
     # as initial D, use stationary distribution for s, plus uniform over a
     pi = stationary_markov(Pi)
     D = pi[:, np.newaxis] * np.ones_like(a_grid) / len(a_grid)
-    
+
     # now iterate until convergence to acceptable threshold
     for it in range(10_000):
         D_new = forward_iteration(D, Pi, a_i, a_pi)
@@ -251,13 +268,26 @@ def distribution_ss(Pi, a, a_grid, tol=1E-10):
 
 """Part 4: solving for steady state, including aggregates"""
 
+
 # ALMOST NO CHANGE (calling interpolate_lottery_loop instead of get_lottery)
 def steady_state(Pi, a_grid, y, r, beta, eis):
     Va, a, c = policy_ss(Pi, a_grid, y, r, beta, eis)
     D = distribution_ss(Pi, a, a_grid)
     a_i, a_pi = interpolate_lottery_loop(a, a_grid)
-    
-    return dict(D=D, Va=Va, 
-                a=a, c=c, a_i=a_i, a_pi=a_pi,
-                A=np.vdot(a, D), C=np.vdot(c, D),
-                Pi=Pi, a_grid=a_grid, y=y, r=r, beta=beta, eis=eis)
+
+    return dict(
+        D=D,
+        Va=Va,
+        a=a,
+        c=c,
+        a_i=a_i,
+        a_pi=a_pi,
+        A=np.vdot(a, D),
+        C=np.vdot(c, D),
+        Pi=Pi,
+        a_grid=a_grid,
+        y=y,
+        r=r,
+        beta=beta,
+        eis=eis,
+    )
--- Tutorials/estimation/model.py
+++ Tutorials/estimation/model.py
@@ -11,21 +11,28 @@ import sequence_jacobian as sj
 hh = sj.hetblocks.hh_sim.hh
 make_grids = sj.hetblocks.hh_sim.make_grids
 
+
 def income(Y, T, e_grid):
     # post-tax labor income
-    y = (Y-T) * e_grid
+    y = (Y - T) * e_grid
     return y
+
 
 household_simple = hh.add_hetinputs([make_grids, income])
 
 
 """Inflation, monetary, and fiscal"""
 
+
 @sj.simple
 def nkpc(pi, Y, X, C, kappa_w, vphi, frisch, markup_ss, eis, beta):
     piw = pi + X - X(-1)
     # note: for simplicity, we ignore distortionary effect of taxation here
-    piwres = kappa_w * (vphi*(Y/X)**(1/frisch) - 1/markup_ss * X * C**-(1/eis)) + beta * piw(1) - piw
+    piwres = (
+        kappa_w * (vphi * (Y / X) ** (1 / frisch) - 1 / markup_ss * X * C ** -(1 / eis))
+        + beta * piw(1)
+        - piw
+    )
     return piwres, piw
 
 
@@ -42,7 +49,7 @@ def ex_post_rate(r_ante):
     return r
 
 
-@sj.solved(unknowns={'B': (-1., 1.)}, targets=['Bres'], solver="brentq")
+@sj.solved(unknowns={"B": (-1.0, 1.0)}, targets=["Bres"], solver="brentq")
 def fiscal_deficit_Trule(r, G, B, Tss, phi_T, Y):
     T = Tss + phi_T * (B(-1) - B.ss)
     Bres = (1 + r) * B(-1) + G - T - B
@@ -51,6 +58,7 @@ def fiscal_deficit_Trule(r, G, B, Tss, phi_T, Y):
 
 """Overall model"""
 
+
 @sj.simple
 def mkt_clearing(A, B, Y, C, G):
     asset_mkt = A - B
@@ -58,5 +66,14 @@ def mkt_clearing(A, B, Y, C, G):
     return asset_mkt, goods_mkt
 
 
-ha = sj.create_model([household_simple, nkpc, monetary_taylor, ex_post_rate, fiscal_deficit_Trule, mkt_clearing],
-                     name="Simple HA Model")
+ha = sj.create_model(
+    [
+        household_simple,
+        nkpc,
+        monetary_taylor,
+        ex_post_rate,
+        fiscal_deficit_Trule,
+        mkt_clearing,
+    ],
+    name="Simple HA Model",
+)
